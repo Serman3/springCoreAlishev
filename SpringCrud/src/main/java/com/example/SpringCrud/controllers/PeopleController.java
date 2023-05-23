253e36de87a4ac7d97e03417b560bb63440b9135
sergei_kozlov_7 <kozlovsergej266@gmail.com>
2023-05-23 23:14:07 +0300

    crud application

---
+++ SpringCrud/src/main/java/com/example/SpringCrud/controllers/PeopleController.java
@@ -0,0 +1,63 @@
+package com.example.SpringCrud.controllers;
+
+import com.example.SpringCrud.dao.PersonDao;
+import com.example.SpringCrud.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    @Autowired
+    private PersonDao personDao;
+
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("people", personDao.index());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", personDao.show(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute Person person){   // ModelAttribute  создант новый объект персон
+       // model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute Person person){
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute Person person){
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+}
