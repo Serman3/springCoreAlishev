8b58a49ebb507335d9bbcbb1603142702c51b8a9
sergei_kozlov_7 <kozlovsergej266@gmail.com>
2023-06-22 21:35:52 +0300

    add PersonDTO and dependency ModelMapper for converts to Person

--- FirstRestApp/src/main/java/com/example/FirstRestApp/controller/PeopleController.java
+++ FirstRestApp/src/main/java/com/example/FirstRestApp/controller/PeopleController.java
@@ -1,45 +1,49 @@
 package com.example.FirstRestApp.controller;
 
+import com.example.FirstRestApp.dto.PersonDTO;
 import com.example.FirstRestApp.model.Person;
 import com.example.FirstRestApp.services.PeopleService;
 import com.example.FirstRestApp.util.PersonErrorResponse;
 import com.example.FirstRestApp.util.PersonNotCreatedException;
 import com.example.FirstRestApp.util.PersonNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public PeopleController(PeopleService peopleService){
+    public PeopleController(PeopleService peopleService, ModelMapper modelMapper){
         this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping()
-    public ResponseEntity<List<Person>> getPeople(){
-        List<Person> people = peopleService.findAll();   // Jackson автоматиески конвертирует эти обЪекты в JSON
+    public ResponseEntity<List<PersonDTO>> getPeople(){
+        List<PersonDTO> people = peopleService.findAll().stream().map(this::convertToPersonDTO).collect(Collectors.toList());   // Jackson автоматиески конвертирует эти обЪекты в JSON
         return ResponseEntity.ok().body(people);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> getPerson(@PathVariable int id){
+    public ResponseEntity<PersonDTO> getPerson(@PathVariable int id){
         Person person = peopleService.findOne(id);
-        return ResponseEntity.ok().body(person);
+        return ResponseEntity.ok().body(convertToPersonDTO(person));
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person, BindingResult bindingResult){     // @RequestBody принимает данные от пользователя, конвертируя в json
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid /*Person person*/ PersonDTO personDTO, BindingResult bindingResult){     // @RequestBody принимает данные от пользователя, конвертируя в json
         if(bindingResult.hasErrors()){
             StringBuilder errorMessage = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -48,7 +52,7 @@ public class PeopleController {
             }
             throw new PersonNotCreatedException(errorMessage.toString());
         }
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO)/*person*/);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
@@ -65,4 +69,17 @@ public class PeopleController {
         // В HTTP ответе тело ответа (response) и статус в заголовке
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    private Person convertToPerson(PersonDTO personDTO){
+        /*Person person = new Person();
+        person.setName(personDTO.getName());
+        person.setAge(personDTO.getAge());
+        person.setEmail(personDTO.getEmail());*/
+        return modelMapper.map(personDTO, Person.class);  // конвертирует поля из дто в персон(так же как делать вручную);
+    }
+
+    private PersonDTO convertToPersonDTO(Person person){
+        return modelMapper.map(person, PersonDTO.class);
+    }
+
 }
--- FirstRestApp/src/main/java/com/example/FirstRestApp/model/Person.java
+++ FirstRestApp/src/main/java/com/example/FirstRestApp/model/Person.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "people")
@@ -28,6 +29,16 @@ public class Person implements Serializable {
     @Email
     @NotEmpty(message = "Email should not be empty")
     private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")           // клиенту не нужно знать про поля createdAt, updated_at, created_who, поэтому будем возвращать PersonDTO
+    private LocalDateTime updated_at;
+
+    @Column(name = "created_who")
+    @NotEmpty(message = "created_who should not be empty")
+    private String created_who;
 
     public Person(){};
     public Person(int id, String name, int age, String email) {
@@ -67,5 +78,29 @@ public class Person implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getCreated_who() {
+        return created_who;
+    }
+
+    public void setCreated_who(String created_who) {
+        this.created_who = created_who;
     }
 }
